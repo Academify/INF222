45e8004babd33c9f3d606a59fa2176f0e1eb0b32
Thiago <thiagofp707@gmail.com>
2022-10-10 23:57:30 -0300

    Second commit

--- main.java
+++ main.java
@@ -1,8 +1,14 @@
 public class Main{
 
   public static void main(String[] args) {
-    final String teste = args[0];
-    System.out.println(teste);
+    
+    int n = args [0];
+    double probA = args[1];
+    double probB = args[2];
+    
+    for(int i = 0; i < n; i++){
+      
+    }
   }
 
 }
